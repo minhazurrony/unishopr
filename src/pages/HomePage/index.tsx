7e40d392d56d089a10e69e5f4d599e171be394ca
Minhazur Rahman <minhazur.rahman@experts.com.bd>
2020-12-06 19:21:43 +0600

    feat: add how it works and pricing section to homepage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,47 +1,23 @@
-import { Image } from '@chakra-ui/core';
-import styled from 'styled-components';
+import { Box, Grid, Icon, Image } from '@chakra-ui/core';
 import { ExternalControlledCarousel } from '../../components/Carousel';
 import { Container } from '../../components/Container';
 import { NavBar } from '../../components/NavBar';
 import { SearchBar } from '../../components/SearchBar';
+import {
+  BigTitle,
+  ContentWrapper,
+  FeatureSection,
+  HeroSection,
+  LogoContainer,
+  SearchContainer,
+  SmallTitle,
+  Tagline,
+  TitleContainer,
+  TitleWithBackground,
+} from './styles/HomeStyled';
+import styles from './styles/homeStyles.module.css';
 
-const HeroSection = styled.section`
-  background: url(/assets/hero.png) no-repeat center;
-  height: 100vh;
-`;
-
-const FeatureSection = styled.section`
-  padding: 40px 20px;
-  background: #1a3445;
-  width: 100%;
-  margin-top: -150px;
-  -webkit-box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
-  -moz-box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
-  box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
-`;
-
-const SearchContainer = styled.div`
-  width: 65%;
-  margin: 0 auto;
-`;
-
-const LogoContainer = styled.div`
-  width: 50%;
-  margin: 20px auto;
-  display: flex;
-  justify-content: center;
-  align-items: baseline;
-`;
-
-const Tagline = styled.h2`
-  color: white;
-  text-align: center;
-  font-size: 20;
-  font-weight: 600;
-  margin-top: 40px;
-`;
-
-export const HomePage = (props: any) => {
+export const HomePage = () => {
   return (
     <>
       <HeroSection>
@@ -51,39 +27,141 @@ export const HomePage = (props: any) => {
 
       <>
         <Container>
-          <FeatureSection>
-            <SearchContainer>
-              <SearchBar placeholder="Product url for request" />
-            </SearchContainer>
-            <LogoContainer>
-              <Image
-                src="assets/amazon-logo.png"
-                width={60}
-                style={{ margin: '0px 15px' }}
-              />
-              <Image
-                src="assets/ebay-logo.png"
-                width={60}
-                style={{ margin: '0px 15px' }}
-              />
-              <Image
-                src="assets/hp-logo.png"
-                width={25}
-                style={{ margin: '0px 15px' }}
-              />
-              <Image
-                src="assets/walmart-logo.png"
-                width={90}
-                style={{ margin: '0px 15px' }}
-              />
-              <Image
-                src="assets/world-logo.png"
-                width={25}
-                style={{ margin: '0px 15px' }}
-              />
-            </LogoContainer>
-            <Tagline>Shop Anywhere, Travel Everywhere</Tagline>
-          </FeatureSection>
+          <ContentWrapper>
+            <FeatureSection>
+              <SearchContainer>
+                <SearchBar placeholder="Product url for request" />
+              </SearchContainer>
+              <LogoContainer>
+                <Image
+                  src="assets/amazon-logo.png"
+                  width={60}
+                  style={{ margin: '0px 15px' }}
+                />
+                <Image
+                  src="assets/ebay-logo.png"
+                  width={60}
+                  style={{ margin: '0px 15px' }}
+                />
+                <Image
+                  src="assets/hp-logo.png"
+                  width={25}
+                  style={{ margin: '0px 15px' }}
+                />
+                <Image
+                  src="assets/walmart-logo.png"
+                  width={90}
+                  style={{ margin: '0px 15px' }}
+                />
+                <Image
+                  src="assets/world-logo.png"
+                  width={25}
+                  style={{ margin: '0px 15px' }}
+                />
+              </LogoContainer>
+              <Tagline>Shop Anywhere, Travel Everywhere</Tagline>
+            </FeatureSection>
+
+            {/* how it works container starts here */}
+            <TitleContainer>
+              <SmallTitle>How it all works?</SmallTitle>
+              <BigTitle>
+                <span className={styles.primary}>Things can't get</span>
+                <span className={styles.secondary}> any simpler!</span>
+              </BigTitle>
+            </TitleContainer>
+            <TitleWithBackground>
+              <h3 className={styles.shopper}>Shopper</h3>
+            </TitleWithBackground>
+
+            <Grid templateColumns="repeat(4, 1fr)">
+              <Box w="100%" p={20} bg="white" className={styles.box}>
+                <Image src="assets/copy.png" className={styles.image} />
+                <p className={styles.description}>
+                  Copy the link of product from any online shop
+                </p>
+              </Box>
+              <Box w="100%" p={20} bg="white" className={styles.box}>
+                <Image src="assets/paste.png" className={styles.image} />
+                <p className={styles.description}>Paste the link in our site</p>
+              </Box>
+              <Box w="100%" p={20} bg="white" className={styles.box}>
+                <Image src="assets/shopping-bag.png" className={styles.image} />
+                <p className={styles.description}>Request for the product</p>
+              </Box>
+              <Box w="100%" p={20} className={styles.lastBox}>
+                <Image
+                  src="assets/shopping-cart.png"
+                  className={styles.image}
+                />
+                <p className={styles.lastDescription}>
+                  Review Price & add to cart
+                </p>
+              </Box>
+            </Grid>
+
+            {/* price container starts here */}
+            <TitleWithBackground style={{ padding: '20px 0' }}>
+              <SmallTitle style={{ color: 'white' }}>
+                Price breakdown
+              </SmallTitle>
+              <BigTitle>
+                <span className={styles.pricePrimary}>We are not</span>
+                <span className={styles.priceSecondary}> overcharging!</span>
+              </BigTitle>
+            </TitleWithBackground>
+            <div className={styles.priceContainer}>
+              <Box
+                bg="rgba(14, 43, 61, 0.9)"
+                w={250}
+                p={10}
+                color="white"
+                className={styles.priceDetailsContainer}
+              >
+                <h2 className={styles.priceTitle}>XXX USD</h2>
+                <p className={styles.priceSubTitle}>X Quantity</p>
+                <h3 className={styles.priceDescription}>
+                  Price of the product
+                </h3>
+              </Box>
+              <Icon name="add" color="primary.500" size="24px" />
+              <Box
+                bg="rgba(14, 43, 61, 0.9)"
+                w={250}
+                p={10}
+                color="white"
+                className={styles.priceDetailsContainer}
+              >
+                <h2 className={styles.priceTitle}>250 TK</h2>
+                <p className={styles.priceSubTitle}>PER KG</p>
+                <h3 className={styles.priceDescription}>Shipping Costs</h3>
+              </Box>
+              <Icon name="add" color="primary.500" size="24px" />
+              <Box
+                bg="rgba(14, 43, 61, 0.9)"
+                w={250}
+                p={10}
+                color="white"
+                className={styles.priceDetailsContainer}
+              >
+                <h2 className={styles.priceTitle}>5 TK</h2>
+                <p className={styles.priceSubTitle}>PER USD</p>
+                <h3 className={styles.priceDescription}>Traveler's Charge</h3>
+              </Box>
+              <Icon name="add" color="primary.500" size="24px" />
+              <Box
+                bg="rgba(14, 43, 61, 0.9)"
+                w={250}
+                p={10}
+                color="white"
+                className={styles.priceDetailsContainer}
+              >
+                <h2 className={styles.priceTitle}>100 TK</h2>
+                <p className={styles.priceSubTitle}>IN DHAKA</p>
+                <h3 className={styles.priceDescription}>Home Delivery</h3>
+              </Box>
+            </div>
+          </ContentWrapper>
         </Container>
       </>
     </>
---
+++ src/pages/HomePage/styles/HomeStyled.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const HeroSection = styled.section`
+  background: url(/assets/hero.png) no-repeat center;
+  height: 100vh;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 100%;
+  box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
+`;
+
+export const FeatureSection = styled.section`
+  padding: 40px 20px;
+  background: #1a3445;
+  width: 100%;
+  margin-top: -150px;
+  -webkit-box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
+  -moz-box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: 0px 1px 31px 0px rgba(0, 0, 0, 0.12);
+`;
+
+export const SearchContainer = styled.div`
+  width: 65%;
+  margin: 0 auto;
+`;
+
+export const LogoContainer = styled.div`
+  width: 50%;
+  margin: 20px auto;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+`;
+
+export const Tagline = styled.h2`
+  color: white;
+  text-align: center;
+  font-size: 20;
+  font-weight: 600;
+  margin-top: 40px;
+`;
+
+export const TitleContainer = styled.div`
+  text-align: center;
+  padding: 50px;
+  line-height: 1.2;
+`;
+
+export const SmallTitle = styled.h3`
+  font-weight: 600;
+  font-size: 14px;
+  color: #494947;
+  opacity: 0.5;
+`;
+
+export const BigTitle = styled.h2`
+  font-weight: 700;
+  font-size: 30px;
+`;
+
+export const TitleWithBackground = styled.div`
+  padding: 10px;
+  background-color: #d1545b;
+  text-align: center;
+`;
